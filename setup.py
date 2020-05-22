902135cc8281df76cca5e3ffe1e750e995a01520
calekochenour <cale.kochenour@alumni.psu.edu>
2020-05-22 16:23:15 -0600

    Add rtree

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ if __name__ == "__main__":
             "shapely",
             "rasterstats",
             "descartes",
+            "rtree",
         ],
         zip_safe=False,
         classifiers=[
