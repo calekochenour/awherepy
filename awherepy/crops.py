44158ecf82c4d8d42be8f46674966bd9a63056db
calekochenour <cale.kochenour@alumni.psu.edu>
2020-06-09 19:33:55 -0600

    Re-structure crops module

--- awherepy/crops.py
+++ awherepy/crops.py
@@ -4,200 +4,183 @@ awherepy.crops
 A module to access and retrieve aWhere crop data.
 """
 
-import requests
+import requests as rq
 import pandas as pd
 from pandas.io.json import json_normalize
-from awherepy.agronomics import Agronomics
+import awherepy as aw
+
+# Define valid crops
+CROPS_LIST = [
+    "barley-generic",
+    "canola-b-napus",
+    "canola-b-rapa",
+    "canola-generic",
+    "corn-2300-gdd",
+    "corn-2500-gdd",
+    "corn-2700-gdd",
+    "corn-2900-gdd",
+    "cotton-2200-gdd",
+    "cotton-2600-gdd",
+    "green-gram-generic",
+    "oat-generic",
+    "potato-generic",
+    "sorghum-long-season",
+    "sorghum-short-season",
+    "soya-generic",
+    "sugarbeet-generic",
+    "sunflower-early-dwarf",
+    "sunflower-generic",
+    "wheat-generic",
+    "wheat-hardred",
+]
+
+# Define variables for data cleaning
+CROPS_DROP_COLS = [
+    "_links.self.href",
+    "_links.curies",
+    "_links.awhere:plantings.href",
+]
+
+CROPS_RENAME_MAP = {
+    "id": "crop_id",
+    "name": "crop_name",
+    "type": "crop_type",
+    "variety": "crop_variety",
+    "isDefaultForCrop": "default_crop",
+}
 
 
-class AgronomicsCrops(Agronomics):
-    def __init__(
-        self,
-        api_key,
-        api_secret,
-        base_64_encoded_secret_key=None,
-        auth_token=None,
-        api_url=None,
-    ):
+def get_crops(
+    key, secret, crop_id=None,
+):
+    """Retrieve a list of available crops or single crop if specified.
 
-        super(AgronomicsCrops, self).__init__(
-            api_key,
-            api_secret,
-            base_64_encoded_secret_key,
-            auth_token,
-            api_url,
-        )
+    API reference:
+        https://docs.awhere.com/knowledge-base-docs/crop-list-detail/
 
-        self.api_url = f"{self.api_url}/crops"
+    Parameters
+    ----------
+    key : str
+        API key for a valid aWhere API application.
 
-    # field_id=None, crop_name=None, limit=10, offset=0
-    def get(self, crop_id=None, limit=10, offset=0):
-        """Retrieve a list of available crops.
-        """
-        # Setup the HTTP request headers
-        auth_headers = {"Authorization": f"Bearer {self.auth_token}"}
+    secret : str
+        API secret for a valid aWhere API application.
 
-        # Get API response, single crop or page of crops
-        response = (
-            requests.get(f"{self.api_url}/{crop_id}", headers=auth_headers)
-            if crop_id
-            else requests.get(
-                f"{self.api_url}?limit={limit}&offset={offset}",
-                headers=auth_headers,
-            )
-        )
+    crop_id : str, optional
+        Valid aWhere crop id.
 
-        # Convert API response to JSON format
-        response_json = response.json()
+    Returns
+    -------
+    crop_df : pandas dataframe
+        Dataframe containing a single crop or all available crops.
 
-        # Convert to dataframe
-        response_df = (
-            json_normalize(response_json)
-            if crop_id
-            else json_normalize(response_json.get("crops"))
-        )
+    Example
+    -------
+        >>> # Imports
+        >>> import os
+        >>> import awherepy as aw
+        >>> import awherepy.fields as awc
+        >>> # Get aWhere API key and secret
+        >>> awhere_api_key = os.environ.get('AWHERE_API_KEY')
+        >>> awhere_api_secret = os.environ.get('AWHERE_API_SECRET')
+        >>> # Get all available aWhere crops
+        >>> all_crops = awc.get_crops(awhere_api_key, awhere_api_secret)
+        >>> # Check number of entries
+        >>> len(all_crops)
+        21
+        >>> # Get a specific crop
+        >>> corn_2500_gdd = awc.get_crops(
+        >>>     awhere_api_key, awhere_api_secret, crop_id='corn-2500-gdd'
+        >>> )
+        >>> Check number of entries
+        >>> len(corn_2500_gdd)
+        1
+    """
+    # Define global variables
+    global CROPS_LIST, CROPS_DROP_COLS, CROPS_RENAME_MAP
 
-        # Drop unnecessary columns
-        response_df.drop(
-            columns=[
-                "_links.self.href",
-                "_links.curies",
-                "_links.awhere:plantings.href",
-            ],
-            inplace=True,
-        )
+    # Define crops api url
+    api_url = "https://api.awhere.com/v2/agronomics/crops"
 
-        # Reset index
-        response_df.reset_index(drop=True, inplace=True)
+    # Check if credentials are valid
+    if aw.valid_credentials(key, secret):
 
-        # Define new column names
-        crop_rename = {
-            "id": "crop_id",
-            "name": "crop_name",
-            "type": "crop_type",
-            "variety": "crop_variety",
-            "isDefaultForCrop": "default_crop",
-        }
+        # Get OAuth token
+        auth_token = aw.get_oauth_token(key, secret)
 
-        # Rename columns
-        response_df.rename(columns=crop_rename, inplace=True)
+        # Set up the HTTP request headers
+        auth_headers = {"Authorization": f"Bearer {auth_token}"}
 
-        return response_df
+        # Check if single crop
+        if crop_id:
 
-    def get_full(self, limit=10, offset=0, max_pages=3):
-        """Retrieves the full list of available crops,
-        based on limit, offset, and max pages.
-        """
-        # Setup the HTTP request headers
-        auth_headers = {"Authorization": f"Bearer {self.auth_token}"}
+            # Raise error if crop id is invalid
+            if crop_id not in CROPS_LIST:
+                raise KeyError(
+                    (
+                        f"Invalid crop id."
+                        f"Valid crop ids: {', '.join(CROPS_LIST)}"
+                    )
+                )
 
-        # Define list to store page dataframes
-        response_df_list = []
+            # Get API response
+            response = rq.get(f"{api_url}/{crop_id}", headers=auth_headers)
 
-        # Loop through all pages
-        while offset < limit * max_pages:
-
-            # Get API response; convert response to dataframe; append
-            #  to dataframe list
-            response = requests.get(
-                f"{self.api_url}?limit={limit}&offset={offset}",
-                headers=auth_headers,
-            )
+            # Convert API response to JSON format
             response_json = response.json()
-            response_df_loop = json_normalize(response_json.get("crops"))
-            response_df_list.append(response_df_loop)
-            offset += 10
 
-        # Merge all dataframes into a single dataframe
-        response_df = pd.concat(response_df_list, axis=0)
+            # Convert to dataframe
+            crop_df = json_normalize(response_json)
 
-        # Drop unnecessary dataframe columns
-        response_df.drop(
-            columns=[
-                "_links.self.href",
-                "_links.curies",
-                "_links.awhere:plantings.href",
-            ],
-            inplace=True,
-        )
+        # All crops
+        else:
 
-        # Reset dataframe index
-        response_df.reset_index(drop=True, inplace=True)
+            # Initialize variables
+            offset = 0
+            limit = 10
+            pages = 3
 
-        # Define new column name mapping
-        crop_rename = {
-            "id": "crop_id",
-            "name": "crop_name",
-            "type": "crop_type",
-            "variety": "crop_variety",
-            "isDefaultForCrop": "default_crop",
-        }
+            # Initialize list to store dataframe for each page of results
+            response_df_list = []
 
-        # Rename dataframe columns
-        response_df.rename(columns=crop_rename, inplace=True)
+            # Loop through all pages
+            while offset < limit * pages:
 
-        return response_df
+                # Get API response
+                response = rq.get(
+                    f"{api_url}?limit={limit}&offset={offset}",
+                    headers=auth_headers,
+                )
 
+                # Convert API response to JSON format
+                response_json = response.json()
 
-class AgronomicsCrop(AgronomicsCrops):
-    def __init__(
-        self,
-        api_key,
-        api_secret,
-        crop_id,
-        base_64_encoded_secret_key=None,
-        auth_token=None,
-        api_url=None,
-    ):
+                # Convert json to dataframe
+                response_df_loop = json_normalize(response_json.get("crops"))
 
-        super(AgronomicsCrop, self).__init__(
-            api_key,
-            api_secret,
-            base_64_encoded_secret_key,
-            auth_token,
-            api_url,
-        )
+                # Append to dataframe list
+                response_df_list.append(response_df_loop)
 
-        self.api_url = f"{self.api_url}/{crop_id}"
+                # Increase offset by 1 page of results
+                offset += limit
 
-    def get(self):
-        """Retrieves the information for the defined crop.
-        """
-        # Setup the HTTP request headers
-        auth_headers = {"Authorization": f"Bearer {self.auth_token}"}
-
-        # Get API response, single crop
-        response = requests.get(f"{self.api_url}", headers=auth_headers)
-
-        # Convert API response to JSON format
-        response_json = response.json()
-
-        # Convert to dataframe
-        response_df = json_normalize(response_json)
+            # Merge all page dataframes into a single dataframe
+            crop_df = pd.concat(response_df_list, axis=0)
 
         # Drop unnecessary columns
-        response_df.drop(
-            columns=[
-                "_links.self.href",
-                "_links.curies",
-                "_links.awhere:plantings.href",
-            ],
-            inplace=True,
+        crop_df.drop(
+            columns=CROPS_DROP_COLS, inplace=True,
         )
 
         # Reset index
-        response_df.reset_index(drop=True, inplace=True)
-
-        # Define new column names
-        crop_rename = {
-            "id": "crop_id",
-            "name": "crop_name",
-            "type": "crop_type",
-            "variety": "crop_variety",
-            "isDefaultForCrop": "default_crop",
-        }
+        crop_df.reset_index(drop=True, inplace=True)
 
         # Rename columns
-        response_df.rename(columns=crop_rename, inplace=True)
+        crop_df.rename(columns=CROPS_RENAME_MAP, inplace=True)
 
-        return response_df
+    # Invalid credentials
+    else:
+        # Raise error
+        raise ValueError("Invalid aWhere API credentials.")
+
+    return crop_df
